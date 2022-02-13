3505a60525ef74f0022a42be9f0cff157a7c6831
Matheus Rocco <matheus.rocco@eemovel.com>
2022-02-13 10:09:44 -0300

    feat: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -19,6 +19,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
+    password: '',
     errorMessage: '',
     emailError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório',
@@ -30,6 +31,12 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
       email: state.email
     })
   }, [state.email])
+
+  useEffect(() => {
+    validation.validate({
+      password: state.password
+    })
+  }, [state.password])
 
   return (
     <div className={Styles.login}>
