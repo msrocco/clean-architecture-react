481fd2a6d3b602588716f740347f6318155ef7cf
Matheus Rocco <matheus.rocco@eemovel.com>
2022-02-13 09:40:39 -0300

    feat: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import { LoginHeader, Input, Footer, FormStatus } from '@/presentation/components'
-import Context from '@/presentation/context/form/formContenxt'
+import {
+  LoginHeader,
+  Input,
+  Footer,
+  FormStatus
+} from '@/presentation/components';
+import Context from '@/presentation/context/form/formContenxt';
 
-import Styles from './login-styles.scss'
+import Styles from './login-styles.scss';
 
 const Login: React.FC = () => {
   const [state] = useState({
     isLoading: false,
     errorMessage: ''
-  })
+  });
 
   return (
     <div className={Styles.login}>
@@ -21,9 +26,20 @@ const Login: React.FC = () => {
 
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
 
-          <Input type="password" name="password" placeholder="Digite sua senha" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
 
-          <button className={Styles.submit} type="submit">Entrar</button>
+          <button
+            data-testid="submit"
+            className={Styles.submit}
+            type="submit"
+            disabled
+          >
+            Entrar
+          </button>
 
           <span className={Styles.link}>Criar conta</span>
 
@@ -33,7 +49,7 @@ const Login: React.FC = () => {
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
