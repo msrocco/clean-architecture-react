3a3574a88d71374b62a2c599caf7570275eee175
Matheus Rocco <matheus.rocco@eemovel.com>
2022-02-15 19:32:29 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
+import faker from 'faker'
 import { InvalidFieldError } from '@/validation/errors';
 import { EmailValidation } from '@/validation/validators/email/email-validation';
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const sut = new EmailValidation(faker.random.words())
+    const error = sut.validate(faker.random.words())
     expect(error).toEqual(new InvalidFieldError())
+  });
+
+  test('Should return falsy if email valid', () => {
+    const sut = new EmailValidation(faker.random.words())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   });
 });
