09654cb23fa84f8209116c96bebed166342eefe7
Matheus Rocco <matheus.rocco@eemovel.com>
2022-02-17 17:51:15 -0300

    fix: import

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory';
-import { makeLoginValidation } from '@/main/factories/pages/login/login-validations-factory';
+import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory';
 import { Login } from '@/presentation/pages'
 
 export const makeLogin: React.FC = () => {
