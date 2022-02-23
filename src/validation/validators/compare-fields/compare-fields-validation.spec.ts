9f257c716b5e7fdc405ecb058e51478076e85752
Matheus Rocco <matheus.rocco@eemovel.com>
2022-02-23 11:22:02 -0300

    test: ensure CompareFieldsValidation returns error if validation fails

---
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -0,0 +1,13 @@
+import faker from 'faker'
+import { CompareFieldsValidation } from './compare-fields-validation';
+import { InvalidFieldError } from '@/validation/errors';
+
+const makeSut = (valueToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(faker.database.column(), valueToCompare)
+
+describe('CompareFieldsValidation', () => {
+  test('Should return error if validation fails', () => {
+    const sut = makeSut(faker.random.word())
+    const error = sut.validate(faker.random.word())
+    expect(error).toEqual(new InvalidFieldError())
+  });
+});
