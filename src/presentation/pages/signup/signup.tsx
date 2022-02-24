2e470547e9a018ca67d1378d7131b6558d39a2ab
Matheus Rocco <matheus.rocco@eemovel.com>
2022-02-23 21:25:46 -0300

    feat: ensure SignUp change email status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -20,6 +20,7 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
     isLoading: false,
     name: '',
     nameError: '',
+    email: '',
     emailError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório',
     passwordConfirmationError: 'Campo obrigatório',
@@ -29,9 +30,10 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
   useEffect(() => {
     setState({
       ...state,
-      nameError: validation.validate('name', state.name)
+      nameError: validation.validate('name', state.name),
+      emailError: validation.validate('email', state.email)
     });
-  }, [state.name]);
+  }, [state.name, state.email]);
 
   return (
     <div className={Styles.signup}>
