048b7ed578ccee40d44a25415d68d5b5db30feb0
Matheus Rocco <matheus.rocco@eemovel.com>
2022-02-27 10:52:30 -0300

    refactor: remove generic body type of http-post-client

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -4,16 +4,15 @@ import { HttpStatusCode } from '@/data/protocols/http';
 import { AccountModel } from '@/domain/models';
 import { EmailInUseError, UnexpectedError } from '@/domain/errors';
 import { mockAccountModel, mockAddAcountParams } from '@/domain/test';
-import { AddAccountParams } from '@/domain/usecases';
 import { RemoteAddAccount } from '@/data/usecases/add-account/remote-add-account';
 
 type SutTypes = {
   sut: RemoteAddAccount,
-  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>
+  httpPostClientSpy: HttpPostClientSpy<AccountModel>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy<AddAccountParams, AccountModel>();
+  const httpPostClientSpy = new HttpPostClientSpy<AccountModel>();
   const sut = new RemoteAddAccount(url, httpPostClientSpy);
 
   return {
