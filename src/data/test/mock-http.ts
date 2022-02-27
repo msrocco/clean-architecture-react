e41531ff8866863df7782c1ce967a69d151ed267
Matheus Rocco <matheus.rocco@eemovel.com>
2022-02-27 17:17:30 -0300

    test: ensure RemoteLoadSurveyList throws Unexpected error on 403

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -17,14 +17,19 @@ export class HttpPostClientSpy<R> implements HttpPostClient<R> {
     this.url = params.url;
     this.body = params.body;
 
-    return Promise.resolve(this.response);
+    return this.response;
   }
 }
 
-export class HttpGetClientSpy implements HttpGetClient {
+export class HttpGetClientSpy<R> implements HttpGetClient<R> {
   url: string
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.ok
+  }
 
-  async get (params: HttpGetParams): Promise<void> {
+  async get (params: HttpGetParams): Promise<HttpResponse<R>> {
     this.url = params.url
+
+    return this.response;
   }
 }
---
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -0,0 +1,40 @@
+import faker from 'faker'
+import { HttpGetClientSpy } from '@/data/test';
+import { RemoteLoadSurveyList } from './remote-load-survey-list';
+import { UnexpectedError } from '@/domain/errors';
+import { HttpStatusCode } from '@/data/protocols/http';
+import { SurveyModel } from '@/domain/models';
+
+type SutTypes = {
+  sut: RemoteLoadSurveyList
+  httpGetClientSpy: HttpGetClientSpy<SurveyModel[]>
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy<SurveyModel[]>()
+  const sut = new RemoteLoadSurveyList(url, httpGetClientSpy)
+
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
+describe('RemoteLoadSurveyList', () => {
+  test('Should call HttpGetClient with correct URL', async () => {
+    const url = faker.internet.url()
+    const { sut, httpGetClientSpy } = makeSut(url)
+    await sut.loadAll()
+    expect(httpGetClientSpy.url).toBe(url)
+  });
+
+  test('Should throw UnexpectedError if HttpPostClient returns 403', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+
+    const promise = sut.loadAll();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+});
