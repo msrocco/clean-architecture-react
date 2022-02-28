25cf379a62b1f611b5f0c3edf27d38baa34d4887
Matheus Rocco <matheus.rocco@eemovel.com>
2022-02-28 09:26:21 -0300

    test: ensure AxiosHttpClient calls axios.get with correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { mockPostRequest } from '@/data/test';
+import { mockGetRequest, mockPostRequest } from '@/data/test';
 import { mockAxios, mockHttpResponse } from '@/infra/test';
 import { AxiosHttpClient } from './axios-http-client';
 
@@ -46,6 +46,16 @@ describe('AxiosHttpClient', () => {
 
       const promise = sut.post(mockPostRequest())
       expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
+    });
+  })
+
+  describe('get', () => {
+    test('Should call axios.get with correct values', async () => {
+      const request = mockGetRequest()
+      const { sut, mockedAxios } = makeSut()
+
+      await sut.get(request)
+      expect(mockedAxios.get).toHaveBeenCalledWith(request.url)
     });
   })
 });
