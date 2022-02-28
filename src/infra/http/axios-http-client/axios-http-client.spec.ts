e651155ab81614bf4180de809d15cd4726ca3569
Matheus Rocco <matheus.rocco@eemovel.com>
2022-02-28 09:36:54 -0300

    test: ensure AxiosHttpClient returns correct values on failure

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -72,5 +72,15 @@ describe('AxiosHttpClient', () => {
         body: axiosResponse.data
       })
     });
+
+    test('Should return correct error on axios.get', () => {
+      const { sut, mockedAxios } = makeSut()
+      mockedAxios.get.mockRejectedValueOnce({
+        response: mockHttpResponse()
+      })
+
+      const promise = sut.get(mockPostRequest())
+      expect(promise).toEqual(mockedAxios.get.mock.results[0].value)
+    });
   })
 });
