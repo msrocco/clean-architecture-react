a42bc297be17894a9a07c8ecdbd1a2dde5a5560b
Matheus Rocco <matheus.rocco@eemovel.com>
2022-03-05 09:34:21 -0300

    feat: add SurveyList composition

--- src/main/factories/pages/signup/signup-factory.tsx
+++ src/main/factories/pages/signup/signup-factory.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { makeRemoteAddAccount } from '@/main/factories/usecases/add-account/remote-add-account-factory';
-import { makeSignUpValidation } from '@/main/factories/pages/signup/signup-validation-factory';
+import { makeRemoteAddAccount } from '@/main/factories/usecases';
+import { makeSignUpValidation } from '@/main/factories/pages';
 import { SignUp } from '@/presentation/pages'
 
 export const makeSignUp: React.FC = () => {
