cb8fdd264c3647de26b79dafad1c46e0f285085c
Matheus Rocco <matheus.rocco@eemovel.com>
2022-03-11 22:29:51 -0300

    feat: ensure SurveyResult presents correct initial state

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FlipMove from 'react-flip-move'
-import { Footer, Header, Loading, Calendar } from '@/presentation/components'
+import { LoadSurveyResult } from '@/domain/usecases'
+import { Footer, Header, Loading, Calendar, Error } from '@/presentation/components'
 import Styles from './survey-result-styles.scss'
 
 const SurveyResult: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    error: '',
+    surveyResult: null as LoadSurveyResult.Model
+  })
+
   return (
     <div className={Styles.surveyResultWrap}>
       <Header />
-      <div className={Styles.contentWrap}>
-        {true && (
+      <div data-testid="survey-result" className={Styles.contentWrap}>
+        {state.surveyResult && (
           <>
             <hgroup>
               <Calendar date={new Date()} className={Styles.calendarWrap} />
@@ -35,7 +42,8 @@ const SurveyResult: React.FC = () => {
           </>
         )}
 
-        { false && <Loading /> }
+        { state.isLoading && <Loading /> }
+        { state.error && <Error error={state.error} reload={() => null} /> }
       </div>
       <Footer />
     </div>
