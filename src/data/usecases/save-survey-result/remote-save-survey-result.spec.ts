3de8b2d39b35599d0580236adec313ecd9088695
Matheus Rocco <matheus.rocco@eemovel.com>
2022-03-12 12:35:05 -0300

    test: ensure RemoteSaveSurveyResult returns AccessDeniedError on 403

--- src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientSpy, mockRemoteSurveyResultModel } from '@/data/test';
 import { RemoteSaveSurveyResult } from '@/data/usecases';
 import { HttpStatusCode } from '@/data/protocols/http';
 import { mockSaveSurveyResultParams } from '@/domain/test';
+import { AccessDeniedError } from '@/domain/errors';
 
 type SutTypes = {
   sut: RemoteSaveSurveyResult
@@ -33,5 +34,15 @@ describe('RemoteSaveSurveyResult', () => {
     expect(httpClientSpy.url).toBe(url)
     expect(httpClientSpy.method).toBe('put')
     expect(httpClientSpy.body).toEqual(saveSurveyResultParams)
+  });
+
+  test('Should throw AccessDeniedError if HttpClient returns 403', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+
+    const promise = sut.save(mockSaveSurveyResultParams())
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   });
 })
