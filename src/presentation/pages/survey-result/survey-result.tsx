7f551e3725dcc3a88315432d4b10bcd1c6a6fd0b
Matheus Rocco <matheus.rocco@eemovel.com>
2022-03-12 10:24:12 -0300

    feat: ensure SurveyResult present error if LoadSurveyResult fails

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import FlipMove from 'react-flip-move'
 import { LoadSurveyResult } from '@/domain/usecases'
 import { Footer, Header, Loading, Calendar, Error } from '@/presentation/components'
+import { useErrorHandler } from '@/presentation/hooks'
 import Styles from './survey-result-styles.scss'
 
 type Props = {
@@ -15,10 +16,14 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
     surveyResult: null as LoadSurveyResult.Model
   })
 
+  const handleError = useErrorHandler((error: Error) => {
+    setState(old => ({ ...old, surveyResult: null, error: error.message }))
+  })
+
   useEffect(() => {
     loadSurveyResult.load()
       .then(surveyResult => setState(prevState => ({ ...prevState, surveyResult })))
-      .catch()
+      .catch(handleError)
   }, [])
 
   return (
